20095c004d949661dfa157c3706bd618ebb3b33a
unknown-user <alex.wirz@gmail.com>
2014-04-11 20:10:38 +0200

    Solution and projects + first test

---
+++ Pipe/Pipe.Tests/StreamTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Pipe.Tests
+{
+	[TestFixture ()]
+	public class StreamTests
+	{
+		[Test ()]
+		public void CanWriteToPipeStream ()
+		{
+			var pipeStream = new PipeStream ();
+			Assert.IsTrue (pipeStream.CanWrite);
+		}
+	}
+}
+
---
+++ Pipe/Pipe/PipeStream.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Pipe
+{
+	public class PipeStream : Stream
+	{
+		public PipeStream ()
+		{
+		}
+
+		#region implemented abstract members of Stream
+
+		public override void Flush ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override int Read (byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override long Seek (long offset, SeekOrigin origin)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override void SetLength (long value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override void Write (byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override bool CanRead {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public override bool CanSeek {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public override bool CanWrite {
+			get {
+				return true;
+			}
+		}
+
+		public override long Length {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public override long Position {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		#endregion
+	}
+}
+
